cc41b83c404caae0f0acd50637d1cfbcd15065db
Artem <awrer1234eads@gmail.com>
2022-02-16 23:38:14 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 import { getCityData } from './requests.js';
+import { UI_ELEMENTS, renderMessage } from './view.js';
 
+UI_ELEMENTS.MESSAGE_FORM.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const messageValue = document.querySelector('.post__message').value;
+  renderMessage(messageValue);
+
+  UI_ELEMENTS.MESSAGE_FORM.reset();
+});
--- js/view.js
+++ js/view.js
@@ -1,4 +1,14 @@
 export const UI_ELEMENTS = {
   MODALS: document.querySelectorAll('.overlay'),
   MODAL_TOGGLES: document.querySelectorAll('[data-path]'),
+  MESSAGE_FORM: document.querySelector('.chat__post'),
+  CHAT_DISPLAY: document.querySelector('.chat__display'),
+  MAIN_MESSAGE_TEMPLATE: document.querySelector('.main-template'),
+}
+
+export function renderMessage(value) {
+  const message = UI_ELEMENTS.MAIN_MESSAGE_TEMPLATE.content.cloneNode(true);
+  message.querySelector('.message__text').textContent = `Я: ${value}`;
+
+  UI_ELEMENTS.CHAT_DISPLAY.append(message);
 }
