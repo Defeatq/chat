b952728a5bea3d5f98dfce7a287f20314cbab60b
Artem <awrer1234eads@gmail.com>
2022-02-19 23:08:10 +0300

    Update

--- js/auth.js
+++ js/auth.js
@@ -4,16 +4,16 @@ import Cookies from 'js-cookie';
 export function checkValidToken(callback, errorHandler) {
   if (Cookies.get('token')) {
     requestUser(Cookies.get('token'))
-    .then(response => response.json())
-    .then(userData => {
-      const isCodeValid = userData.token === Cookies.get('token');
+      .then(response => response.json())
+      .then(userData => {
+        const isCodeValid = userData.token === Cookies.get('token');
 
-      if (isCodeValid) {
-        callback(userData);
-      } else {
-        throw new Error('Invalid code');
-      }
-    })
-    .catch(errorHandler)
+        if (isCodeValid) {
+          callback(userData);
+        } else {
+          throw new Error('Invalid code');
+        }
+      })
+      .catch(errorHandler)
   }
 }
